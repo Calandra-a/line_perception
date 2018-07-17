2930e26a14f4bc4539bf1e64c1eff2098cebfe78
Calandra-a <acalandra@oakland.edu>
2018-07-17 16:00:18 -0400

    moar

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,29 +1,61 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "iostream"
+ 
 using namespace cv;
+using namespace std;
 int main(int argc, char** argv )
 {
-   /* if ( argc != 2 )
-    {
-        printf("usage: load.out ~/Pictures/image.png\n");
-        return -1;
-    }*/
+    //loads rgb image
     Mat image;
     image = imread( argv[1], 1 );
-/*    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }*/
 
-    //Converting the Image into GrayScale and Storing it in a Matrix 'img_gray'
+    //convert rgb to gray
     Mat img_gray;
     img_gray = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+    
+    //convert gray to bw
+    Mat img_bw;
+    img_bw = img_gray > 230;
 
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
-    namedWindow("Display Image gray", WINDOW_AUTOSIZE);
-    imshow("Display Image gray", img_gray);
-    waitKey(0);
+    //display rgb
+    //namedWindow("Display Image", WINDOW_AUTOSIZE);
+    //imshow("Display Image", image);
+    //display gray
+    //namedWindow("Display Image gray", WINDOW_AUTOSIZE);
+    //imshow("Display Image gray", img_gray);
+    //display bw
+    //namedWindow("Display Image bw", WINDOW_AUTOSIZE);
+    //imshow("Display Image bw", img_bw);
+
+    Mat gray, edge, draw,contourImage;
+    //cvtColor(src1, gray, CV_BGR2GRAY);
+ 
+    Canny( img_bw, edge, 50, 150, 3);
+ 
+    edge.convertTo(draw, CV_8U);
+
+    namedWindow("draw", CV_WINDOW_AUTOSIZE);
+    imshow("draw", draw);
+ 
+
+   /* vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(edge, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE, Point(0,0) );
+    for (unsigned int i=0; i<contours.size(); i++)
+       if (hierarchy[i][3] >= 0)   //has parent, inner (hole) contour of a closed edge (looks good)
+           drawContours(edge, contours, i, Scalar(255, 0, 0), 1, 8);
+           //drawContours(contourImage, contours, i, (0,255,0), 3);
+
+    namedWindow("lines", CV_WINDOW_AUTOSIZE);
+    imshow("lines", contourImage);
+    */
+    
+    waitKey(0);                                       
     return 0;
 }
+ 
+    
