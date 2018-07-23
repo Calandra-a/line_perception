3a54118a11d357e6dc9b31cfc6dba1a34f4e52b0
Calandra-a <acalandra@oakland.edu>
2018-07-23 15:15:00 -0400

    hough transform added

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -31,29 +31,34 @@ int main(int argc, char** argv )
     //namedWindow("Display Image bw", WINDOW_AUTOSIZE);
     //imshow("Display Image bw", img_bw);
 
-    Mat gray, edge, draw,contourImage;
+    Mat gray, edge, draw,contourImage, cdst;
     //cvtColor(src1, gray, CV_BGR2GRAY);
  
     Canny( img_bw, edge, 50, 150, 3);
  
     edge.convertTo(draw, CV_8U);
 
-    namedWindow("draw", CV_WINDOW_AUTOSIZE);
-    imshow("draw", draw);
- 
+    //namedWindow("draw", CV_WINDOW_AUTOSIZE);
+    //imshow("draw", draw);
+    cvtColor(edge, cdst, COLOR_GRAY2BGR);
 
-   /* vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours(edge, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_NONE, Point(0,0) );
-    for (unsigned int i=0; i<contours.size(); i++)
-       if (hierarchy[i][3] >= 0)   //has parent, inner (hole) contour of a closed edge (looks good)
-           drawContours(edge, contours, i, Scalar(255, 0, 0), 1, 8);
-           //drawContours(contourImage, contours, i, (0,255,0), 3);
-
-    namedWindow("lines", CV_WINDOW_AUTOSIZE);
-    imshow("lines", contourImage);
-    */
-    
+    // Standard Hough Line Transform
+    vector<Vec2f> lines; // will hold the results of the detection
+    HoughLines(edge, lines, 1, CV_PI/15, 150,0, 0 ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 800*(-b));
+        pt1.y = cvRound(y0 + 800*(a));
+        pt2.x = cvRound(x0 - 800*(-b));
+        pt2.y = cvRound(y0 - 800*(a));
+        line(cdst, pt1, pt2, Scalar(255,255,255), 3, CV_AA);
+    }
+    imshow("Detected Lines (in white) - Standard Hough Line Transform", cdst);
     waitKey(0);                                       
     return 0;
 }
