8a0edaa3f97d30dc872f785f6edb552f88a6ea35
Calandra-a <acalandra@oakland.edu>
2018-07-25 10:33:13 -0400

    hough transform on curve

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv )
     
     //convert gray to bw
     Mat img_bw;
-    img_bw = img_gray > 220;
+    img_bw = img_gray > 230;
 
     //display rgb
     //namedWindow("Display Image", WINDOW_AUTOSIZE);
@@ -43,7 +43,7 @@ int main(int argc, char** argv )
     cvtColor(edge, cdst, COLOR_GRAY2BGR);
 
     // Standard Hough Line Transform
-    vector<Vec2f> lines; // will hold the results of the detection
+    /*vector<Vec2f> lines; // will hold the results of the detection
     HoughLines(edge, lines, 1, CV_PI/1, 150,0, 0 ); // runs the actual detection
     // Draw the lines
     for( size_t i = 0; i < lines.size(); i++ )
@@ -57,7 +57,17 @@ int main(int argc, char** argv )
         pt2.x = cvRound(x0 - 1000*(-b));
         pt2.y = cvRound(y0 - 1000*(a));
         line(cdst, pt1, pt2, Scalar(255,255,255), 3, CV_AA);
+    }*/
+        // Probabilistic Line Transform
+    vector<Vec4i> linesP; // will hold the results of the detection
+    HoughLinesP(edge, linesP, 1, CV_PI/10, 10, 50, 35 ); // runs the actual detection
+    // Draw the lines
+    for( size_t i = 0; i < linesP.size(); i++ )
+    {
+        Vec4i l = linesP[i];
+        line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(255,255,255), 3, LINE_AA);
     }
+
     imshow("Detected Lines (in white) - Standard Hough Line Transform", cdst);
     waitKey(0);                                       
     return 0;
